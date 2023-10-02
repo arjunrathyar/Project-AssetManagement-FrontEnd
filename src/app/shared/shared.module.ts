6ba26df9c5bbe4a05a2834614ce4fd6c85085f55
arjunrathyar <arjunrathyar@am.students.amrita.edu>
2023-10-02 11:04:11 +0530

    Added Bootstrap and Page Not Found

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
-import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
-  declarations: [SharedComponent, HomeComponent, PageNotFoundComponent],
+  declarations: [SharedComponent, PageNotFoundComponent],
   imports: [
     CommonModule,
     SharedRoutingModule

a4021d22d80bd8dec70706041fd1b6a4d77ae40d
arjunrathyar <arjunrathyar@am.students.amrita.edu>
2023-10-02 10:54:12 +0530

    Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PageNotFoundComponent } from './shared/page-not-found/page-not-found.component';
+import { AuthComponent } from './auth/auth.component';
+import { SharedComponent } from './shared/shared.component';
+import { PurchaseOrderComponent } from './purchase-order/purchase-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //Empty Routiing
+  {
+    path:'',redirectTo:'auth/login', pathMatch: 'full'
+  },
+
+  //lazy loading
+
+  {
+    path: "purchase-order",
+    component: PurchaseOrderComponent,
+    loadChildren: () => import('./purchase-order/purchase-order.module').then(x => x.PurchaseOrderModule)
+  },
+
+  {
+    path: "shared",
+    component: SharedComponent,
+    loadChildren: () => import('./shared/shared.module').then(x => x.SharedModule)
+  },
+
+  {
+    path: "auth",
+    component: AuthComponent,
+    loadChildren: () => import('./auth/auth.module').then(x => x.AuthModule)
+  },
+
+  //Wild card routes
+  {
+    path: "**",
+    component: PageNotFoundComponent
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [],

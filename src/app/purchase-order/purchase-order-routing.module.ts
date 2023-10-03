0a292b55b2be02eeb405992032b7d03dad50a7a5
arjunrathyar <arjunrathyar@am.students.amrita.edu>
2023-10-03 06:21:43 +0530

    purchase order routing done for sub components

--- src/app/purchase-order/purchase-order-routing.module.ts
+++ src/app/purchase-order/purchase-order-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddPurchaseOrderComponent } from './add-purchase-order/add-purchase-order.component';
+import { EditPurchaseOrderComponent } from './edit-purchase-order/edit-purchase-order.component';
+import { ListPurchaseOrderComponent } from './list-purchase-order/list-purchase-order.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  
+  {path:"add",component:AddPurchaseOrderComponent},
+  {path:"edit/:id",component: EditPurchaseOrderComponent},
+  {path:"list",component: ListPurchaseOrderComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

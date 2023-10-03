478595ce5fb89ccd7804a5c49ce8ed742807e3a0
arjunrathyar <arjunrathyar@am.students.amrita.edu>
2023-10-03 06:16:01 +0530

    purchase order sub components added

--- src/app/purchase-order/purchase-order.module.ts
+++ src/app/purchase-order/purchase-order.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { PurchaseOrderRoutingModule } from './purchase-order-routing.module';
 import { PurchaseOrderComponent } from './purchase-order.component';
+import { AddPurchaseOrderComponent } from './add-purchase-order/add-purchase-order.component';
+import { EditPurchaseOrderComponent } from './edit-purchase-order/edit-purchase-order.component';
+import { ListPurchaseOrderComponent } from './list-purchase-order/list-purchase-order.component';
 
 
 @NgModule({
-  declarations: [PurchaseOrderComponent],
+  declarations: [PurchaseOrderComponent, AddPurchaseOrderComponent, EditPurchaseOrderComponent, ListPurchaseOrderComponent],
   imports: [
     CommonModule,
     PurchaseOrderRoutingModule

7d4474597a7282975efe2c6688d23f1e2d3804dd
arjunrathyar <arjunrathyar@am.students.amrita.edu>
2023-10-03 21:54:53 +0530

    Listing Purchase Orders

--- src/app/purchase-order/list-purchase-order/list-purchase-order.component.ts
+++ src/app/purchase-order/list-purchase-order/list-purchase-order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { PurchaseOrderService } from "../../shared/services/purchase-order.service";
 
 @Component({
   selector: 'app-list-purchase-order',
@@ -7,9 +9,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPurchaseOrderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public purchaseOrderService: PurchaseOrderService, private router: Router) { }
 
   ngOnInit(): void {
+    console.log("In PurchaseOrders-List component")
+    this.purchaseOrderService.getAllPurchaseOrdersList();
   }
 
 }

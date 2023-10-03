95774e41393c17217ae5120771bb969af6ef58d8
DivyaRevindran <divyarevindran73@gmail.com>
2023-10-03 23:14:56 +0530

    add and list of Asset-Master added

--- src/app/asset-master/asset-add/asset-add.component.ts
+++ src/app/asset-master/asset-add/asset-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import {AssetMasterService } from 'src/app/shared/services/asset-master.service'
+import {AssetMasterService } from 'src/app/shared/services/asset-master.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-asset-add',
@@ -14,5 +15,30 @@ export class AssetAddComponent implements OnInit {
 
   ngOnInit(): void {
   }
+ //insert
+ addAsset(form: NgForm) {
+  console.log('Inserting...');
+  this.assetMasterService.insertAsset(form.value).subscribe(
+    (response) => {
+      console.log(response);
+
+    },
+    (error) => {
+      console.log(error);
+
+    }
+  )
+
+}
+
+//submit form
+onSubmit(form:NgForm) {
+  console.log(form.value);
+  //Insert
+  this.addAsset(form);
+  form.resetForm();
+  this.router.navigate(["/assets/list"])
+  
+}
 
 }
--- src/app/shared/services/asset-master.service.ts
+++ src/app/shared/services/asset-master.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AssetMaster } from '../models/asset-master';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +19,32 @@ export class AssetMasterService {
 
 
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  //1. Get all employees - promises
+
+  getAllAssets(): void {
+    this.httpClient.get(environment.apiUrl + '/api/asset')
+      .toPromise()
+      .then(response => {
+        console.log(response);
+        this.assets = response as AssetMaster[]
+
+      },
+        error => {
+          console.log(error);
+
+        });
+  }
+
+  //2.get all employees - observable types
+  getAllAssetList(): Observable<any> {
+    return this.httpClient.get(environment.apiUrl + '/api/assets')
+  }
+
+  //3.Insert
+  insertAsset(assetMaster:AssetMaster): Observable<any>{
+      return this.httpClient.post(environment.apiUrl + '/api/asset',assetMaster);
+  }
+
+   
 }

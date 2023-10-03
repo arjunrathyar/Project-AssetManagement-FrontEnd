965be07abed27ef5b91fcb51694ccf84958409f0
arjunrathyar <arjunrathyar@am.students.amrita.edu>
2023-10-04 03:22:12 +0530

    purchase order listing

--- src/app/shared/models/purchase-order.ts
+++ src/app/shared/models/purchase-order.ts
@@ -1,18 +1,21 @@
+import { AssetDefinition, AssetType } from "./asset-definition.model";
+import { PurchaseStatus } from "./purchase-status";
+
 export class PurchaseOrder {
 
     id : number = 0;
     assetPurchaseOrderNumber : number = 0;
     assetDefId: number = 0;
-    //assetDefinition: AssetDefinition;
+    assetDefinition: AssetDefinition;
     assetTypeId: number = 0;
-    //assetType: AssetType;
+    assetType: AssetType;
     assetQuantity: number = 0;
     assetVendorID: number = 0;
     //vendor: Vendor;
     assetOrderDate: Date;
     assetDeliveryDate: Date;
     assetStatusID: number;
-    //status: AssetStatus;
+    status: PurchaseStatus;
     active: boolean = false;
 
 }

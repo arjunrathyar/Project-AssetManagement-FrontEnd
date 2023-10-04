98ea9a12133c3c102e43b98629731bfd9336ff13
arjunrathyar <arjunrathyar@am.students.amrita.edu>
2023-10-05 00:55:41 +0530

    guard and interceptor completed

--- src/app/auth/admin-dashboard/admin-dashboard.component.ts
+++ src/app/auth/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminDashboardComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  // Function to logout
+  logout(): void {
+    // Clear any authentication tokens, etc. (if applicable)
+    // Then navigate to the login page
+    this.router.navigate(['auth/login']);
+  }
+
+  // Function to navigate based on the button clicked
+  navigateTo(destination: string): void {
+    if (destination === 'asset-creation') {
+      this.router.navigate(['/asset-definition']);
+    } else if (destination === 'vendor-creation') {
+      this.router.navigate(['/vendor/list']);
+    } else if (destination === 'approve-reject') {
+      this.router.navigate(['/purchase/list']);
+    }
   }
 
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -3,13 +3,14 @@ import { User } from '../models/user';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient, private router: Router) { }
 
   //verify login username and password
   public loginVerify(user: User): Observable<any> {     //public loginVerify(user: User) dan do this without return type as well
@@ -17,5 +18,17 @@ export class AuthService {
     //call web api for checking username and password
     return this.httpClient.get<User>(environment.apiUrl + "/api/users/" + user.userName + "&" + user.password);
 
+  }
+
+  public logOut(){
+
+    //clear all session and local storage keys
+    localStorage.removeItem("USER_NAME");
+    localStorage.removeItem("ACCESS_ROLE");
+    localStorage.removeItem("JWT_TOKEN");
+    sessionStorage.removeItem("USER_NAME");
+
+    //redirect to Login
+    this.router.navigate(['auth/login']);
   }
 }
